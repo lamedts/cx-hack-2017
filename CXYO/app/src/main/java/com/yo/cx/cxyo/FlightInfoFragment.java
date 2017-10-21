53a0705882ba3b63d3a97c3834255cab8a009280
lamedts <lamedts@gmail.com>
2017-10-22 07:00:22 +0800

    checkpoint #7

--- CXYO/app/src/main/java/com/yo/cx/cxyo/FlightInfoFragment.java
+++ CXYO/app/src/main/java/com/yo/cx/cxyo/FlightInfoFragment.java
@@ -10,10 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 import aero.panasonic.inflight.services.InFlight;
 import aero.panasonic.inflight.services.IInFlightCallback;
@@ -22,17 +26,23 @@ import aero.panasonic.inflight.services.InFlightServices;
 import aero.panasonic.inflight.services.advertising.AdvertisingAttributes;
 import aero.panasonic.inflight.services.advertising.AdvertisingV1;
 import aero.panasonic.inflight.services.advertising.Banner;
-import aero.panasonic.inflight.services.data.datasourcemanager.FileStoreDataSourceManager;
-import aero.panasonic.inflight.services.passengerdata.v1.PassengerData;
+import aero.panasonic.inflight.services.connectinggate.ConnectingFlight;
+import aero.panasonic.inflight.services.connectinggate.ConnectingGateV1;
+import aero.panasonic.inflight.services.connectinggate.CurrentFlight;
 
 
 public class FlightInfoFragment extends Fragment {
 
     private InFlight mInFlight;
     private AdvertisingV1 advertisingV1;
-    private PassengerData mPassengerData;
+    private ConnectingGateV1 connectingGateV1;
 
     private ImageView bannerView;
+    private TextView toLoc;
+    private TextView toLocLong
+    private TextView fromLoc;
+    private TextView fromLocLong
+    private TextView flightNo;
 
     @Nullable
     @Override
@@ -40,6 +50,11 @@ public class FlightInfoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_flight_info, container, false);
         bannerView = (ImageView) view.findViewById(R.id.banner);
+        toLoc = (TextView) view.findViewById(R.id.from_loc);
+        fromLoc = (TextView) view.findViewById(R.id.to_loc);
+        toLocLong = (TextView) view.findViewById(R.id.to_long_loc);
+        fromLocLong = (TextView) view.findViewById(R.id.from_long_loc);
+        flightNo = (TextView) view.findViewById(R.id.flight_no);
 
         mInFlight = ((MainActivity)getActivity()).getIFE();
 
@@ -72,32 +87,50 @@ public class FlightInfoFragment extends Fragment {
             }
         }, mInFlight);
 
-        PassengerData.initService(getContext(), new IInFlightCallback() {
+        ConnectingGateV1.initService(getActivity(), new IInFlightCallback() {
             @Override
             public void onInitServiceComplete(Object o, String s) {
-                if(s.equals(InFlightServices.PASSENGER_DATA_V1_SERVICE.getServiceName())) {
-                    mPassengerData = (PassengerData) o;
-                    if (mPassengerData == null) return;
-
-                    mPassengerData.requestAllPassengersBasicInfo(new PassengerData.OnAllPassengersBasicInfoReceiveListener() {
-                        @Override
-                        public void onPassengersBasicInfoReceived(JSONObject passengerData) {
-                            Log.d("Debug", "onPassengersBasicInfoReceived: " + passengerData.toString());
-                        }
-
-                        @Override
-                        public void onPassengerDataError(PassengerData.Error error) {
-                            Log.e("Error", "onPassengerDataError: " + error);
-                        }
-                    });
+                if(s.equals(InFlightServices.CONNECTING_GATE_V1_SERVICE.getServiceName())){
+                    connectingGateV1 = (ConnectingGateV1) o;
+                    connectingGateV1.requestConnectingGateInfo("",null,"",connectingGateInfoListener);
                 }
             }
+
             @Override
-            public void onInitServiceFailed(String serviceName, InFlight.Error error) {
-                Log.e("Error", "onInitServiceFailed: " + serviceName + " " + error);
+            public void onInitServiceFailed(String s, InFlight.Error error) {
+                Log.e("Error", "onInitServiceFailed()" + InFlight.Error.getErrorMessage(error));
             }
-        }, mInFlight);
+        },mInFlight);
+
 
         return view;
     }
+
+    private final ConnectingGateV1.ConnectingGateInfoListener connectingGateInfoListener = new ConnectingGateV1.ConnectingGateInfoListener(){
+        @Override
+        public void onConnectingGateError(ConnectingGateV1.Error error) {
+            Log.e("Error", "onConnectingGateError" + error.toString());
+        }
+        @Override
+        public void onConnectingGateInfoAvailable(CurrentFlight currentFlight, List<ConnectingFlight> list) {
+            Log.i("Info", "Current Flight Info: " + currentFlight.toString() + "\n");
+            // Log.i("Info", "Current Flight Info: " + currentFlight.get() + "\n");
+
+            toLoc.setText(currentFlight.getArrivalAirport().getIataCode());
+            fromLoc.setText(currentFlight.getDepartureAirport().getIataCode() );
+            flightNo.setText(currentFlight.getFlightNumber());
+            fromLocLong.setText(currentFlight.getDepartureAirport().getCity());
+            toLocLong.setText(currentFlight.getArrivalAirport().getCity());
+
+            if (list.size() > 0) {
+                for(ConnectingFlight connectingFlight : list){
+                    Log.i("Info","Connecting Flight: " + connectingFlight.toString() + "\n");
+                }
+            }else{
+                Log.i("Info","No connecting flights");
+            }
+
+        }
+    };
+
 }
