80fd5d73d53d1d5c190a7912d31fa38fc08e4a4b
lamedts <lamedts@gmail.com>
2017-10-22 00:38:46 +0800

    check point 2

---
+++ CXYO/app/src/main/java/com/yo/cx/cxyo/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.yo.cx.cxyo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void go2Main(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText editText = (EditText) findViewById(R.id.register_display_name);
+        String message = editText.getText().toString();
+        intent.putExtra("DIS_NAME", message);
+        startActivity(intent);
+    }
+}
